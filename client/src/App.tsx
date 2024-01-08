3a5f7b4a5b40c06b79edf9cf0ebb49611cbaa0b8
Maor <maorbz858@gmail.com>
2024-01-08 17:26:56 +0200

    Refactor App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React, { Suspense } from 'react';
 
 import Header from '@layouts/header';
 
-const HomePage = React.lazy(() => import('@pages/HomePage'));
-const AboutPage = React.lazy(() => import('@pages/AboutPage'));
-const SearchPage = React.lazy(() => import('@pages/SearchPage'));
+import HomePage from '@pages/HomePage';
+import AboutPage from '@pages/AboutPage';
+import SearchPage from '@pages/SearchPage';
 
 import '@common/assets/animations/loading.css';
 
@@ -13,13 +12,11 @@ export default function App() {
     return (
         <Router>
             <Header />
-            <Suspense fallback={<div className="fallback-loading pac-man" />}>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="about" element={<AboutPage />} />
-                    <Route path="search" element={<SearchPage />} />
-                </Routes>
-            </Suspense>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="about" element={<AboutPage />} />
+                <Route path="search" element={<SearchPage />} />
+            </Routes>
         </Router>
     );
 }
