376a2fc0e92409cc7bb16c896e1cd729b342d2d0
Maor <maorbz858@gmail.com>
2024-02-03 15:40:15 +0200

    Add lazy loading for page components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
 
 import Header from '@layouts/header';
 
-import HomePage from '@pages/HomePage';
-import AboutPage from '@pages/AboutPage';
-import SearchPage from '@pages/SearchPage';
-
 import '@common/assets/animations/loading.css';
+
+const HomePage = React.lazy(() => import('@pages/HomePage'));
+const AboutPage = React.lazy(() => import('@pages/AboutPage'));
+const SearchPage = React.lazy(() => import('@pages/SearchPage'));
 
 export default function App() {
     return (
         <Router>
             <Header />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="about" element={<AboutPage />} />
-                <Route path="search" element={<SearchPage />} />
-            </Routes>
+            <Suspense fallback={<div className="fallback-loading pac-man" />}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route path="search" element={<SearchPage />} />
+                </Routes>
+            </Suspense>
         </Router>
     );
 }
