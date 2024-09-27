bad06c3861fe483e0027ffc153282c2889dca0d1
Maor <maorbz858@gmail.com>
2024-09-27 18:19:44 +0300

    Refactor throttler configuration in app.module.ts to include a unique key for each section

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { seconds, ThrottlerModule } from '@nestjs/throttler';
 import { RealEstateModule } from './real-estate/real-estate.module';
+import { Request } from 'express';
 
 @Module({
   imports: [
@@ -12,16 +13,19 @@ import { RealEstateModule } from './real-estate/real-estate.module';
         name: 'short',
         ttl: seconds(1),
         limit: 3,
+        getTracker: (req: Request) => req.ips.length ? req.ips[0] : req.ip,
       },
       {
         name: 'medium',
         ttl: seconds(10),
         limit: 5,
+        getTracker: (req: Request) => req.ips.length ? req.ips[0] : req.ip,
       },
       {
         name: 'long',
         ttl: seconds(30),
         limit: 10,
+        getTracker: (req: Request) => req.ips.length ? req.ips[0] : req.ip,
       },
     ]),
     ConfigModule.forRoot({
